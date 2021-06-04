dff7d01b0ee4dcfcb711964413c2bb60ace62ff7
Levent Çelik <lcelik17@ku.edu.tr>
2021-06-04 05:18:29 +0300

    Many new stuff, read the description
    Functions fully implemented.
    Tests fully implemented.
    A prototype algorithm using arithmetic and getting a solution in O(n) time instead of O(nk) time is found and is fully implemented.
    Option to use prototype algorithm is implemented.
    Comparison between the outputs of prototype algorithm and proven algorithm always resulted in equality, for many random inputs.

--- src/Operations.java
+++ src/Operations.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // 0 based indexing for background operations.
 public class Operations {
 	private int n, k;
@@ -10,6 +12,7 @@ public class Operations {
 		totalCost = 0;
 		this.n = n;
 		this.k = k;
+		if(costs.length != n) System.out.println("Entered costs is not of size " + n + ". The program may encounter an ArrayIndexOutOfBoundsException.");
 		this.costs = costs;
 		fillArray();
 	}
@@ -26,6 +29,7 @@ public class Operations {
 	 * if i is out of bounds. Adds to the totalCost the
 	 * cost of changing i.
 	 * @param i		the center index
+	 * @time 		O(k)
 	 */
 	public void change(int i) {
 		if(i < 0 || i >= n) {
@@ -55,6 +59,7 @@ public class Operations {
 	 * Does not print anything.
 	 * @param p			First index to be changed
 	 * @return			Whether starting with p gives a solution
+	 * @time			O(n)
 	 */
 	public boolean solveAtP(int p) {
 		return solveAtP(p, false);
@@ -67,10 +72,11 @@ public class Operations {
 	 * @param p			First index to be changed
 	 * @param print		true to print information about steps
 	 * @return			Whether starting with p gives a solution
+	 * @time			O(n)
 	 */
 	public boolean solveAtP(int p, boolean print) {
 		if(p < 0 || p >= n) {
-			System.out.println("Out of bounds.");
+			if(print) System.out.println("Out of bounds.");
 			return false;
 		}
 		for(int j = p; j < n; j+=2*k+1) {
@@ -79,7 +85,7 @@ public class Operations {
 		boolean solved = string[n-1] == 0; //No bit is changed twice, so if the last bit is 0, we have a solution.
 		if(print) {
 			if(!solved) {
-				System.out.println("Could not solve at index " + p);
+				System.out.println("Could not solve at index " + (p+1));
 				printString();
 			}
 		}
@@ -94,6 +100,7 @@ public class Operations {
 	 * the minimum cost of all valid solutions.
 	 * Does not print anything.
 	 * @return			the minimum cost of the valid solutions
+	 * @time			O(nk)
 	 */
 	public int solve() {
 		return solve(false);
@@ -102,18 +109,88 @@ public class Operations {
 	/**
 	 * Uses solveAtP at each index from 0 to k, finds 
 	 * the minimum cost of all valid solutions.
-	 * @param print		true to print the string after *each* alteration
+	 * @param print		true to print information about steps
 	 * @return			the minimum cost of the valid solutions
+	 * @time			O(nk)
 	 */
 	public int solve(boolean print) {
 		int minCost = Integer.MAX_VALUE;
+		int bestIndex = 0;
 		for(int i = 0; i < k+1; i++) {
 			if(solveAtP(i, print)) {
 				minCost = Math.min(minCost, totalCost);
+				if(minCost == totalCost)
+					bestIndex = i+1;
 			}
 			totalCost = 0;
 		}
+		if(print) System.out.println("Starting at " + bestIndex + " gives the best solution.");
 		return minCost;
 	}
 
+	/**
+	 * Uses prototype algorithm to get a solution. Similar to solve(), but faster.
+	 * @return		the minimum cost of the valid solutions
+	 * @time		Generally O(n), O(n/k) or O(k) for some inputs n, k.
+	 */
+	public int prototypeSolve() {
+		if(n <= k+1) {	//Anything is a solution with a single change action, return the minimum of all costs. This option is O(n).
+			int minCost = Integer.MAX_VALUE;
+			for(int i = 0; i < n; i++) {	//O(n).
+				minCost = Math.min(minCost, costs[i]);
+			}
+			return minCost;
+		}else if( n <= 2*k+1){ // n - 2*(n-k-1) <=> 2*k-n+2  many solutions, with a single flip. This option is O(k).
+			int minCost = Integer.MAX_VALUE;
+			for(int i = n-k-1; i < k+1; i++) 	//O(2k-n+2) for k < n < 2k+1, O(k) for worst n, k (n = 3k/2).
+				minCost = Math.min(minCost, costs[i]);
+			return minCost;
+		}else if(n%(2*k+1) == 0) {	//There's only 1 solution and it starts with index k+1. This option is O(n/k).
+			int totalCost = 0;
+			int index = k;
+			while(index < n) {	//O(n/k).
+				totalCost += costs[index];
+				index+=2*k+1;
+			}
+			return totalCost;
+		}else {	//This option is O(n).
+			int totalSolutions = n%(2*k+1);	// There are that many solutions.
+			if(totalSolutions <= k+1) { // We have a solution starting with 1, and then 2, 3, 4, ..., totalSolutions. This option is O(n).
+				int minCost = Integer.MAX_VALUE;
+				for(int i = 0; i < totalSolutions; i++) {	// Find the cost of each solution and get the minimum, this loop is O(k)
+					int currentCost = 0;
+					for(int j = i; j < n; j+= 2*k+1) {	//O(n/k);
+						currentCost += costs[j];
+					}
+					minCost = Math.min(minCost, currentCost);
+				}
+				return minCost;
+			}else {	//We do not have a solution using the first bit. This option is also O(n).
+				int firstIndex = totalSolutions-k;	//There are not really totalSolutions many solutions in this case, sorry for bad naming.
+				int minCost = Integer.MAX_VALUE;
+				for(int i = firstIndex-1; i < k+1; i++) {	//Iterate over the possible solutions, find the minimum. This loop is O(k).
+					int currentCost = 0;
+					for(int j = i; j < n; j+=2*k+1) { //O(n/k)
+						currentCost += costs[j];
+					}
+					minCost = Math.min(minCost, currentCost);
+				}
+				return minCost;
+			}
+		}
+	}
+	/**
+	 * Uses an algorithm to solve the problem, indicated by the input boolean.
+	 * @param prototype
+	 * @return
+	 */
+	public int optionSolve(boolean prototype) {
+		if(prototype) 
+			return prototypeSolve();
+		return solve(true);
+	}
+	
+	public boolean compareAlgorithms() {
+		return solve() == prototypeSolve();
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -1,43 +1,189 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Test {
 
-
+	private static final int TEST_COUNT = 5;
+	private static boolean prototype = false;
 
 	public static void main(String[] args) {
-//		Scanner scanner = new Scanner(System.in);
-//		while(true) {
-//			System.out.print("Enter n: ");
-//			int n = scanner.nextInt();
-//			System.out.print("Enter k: ");
-//			int k = scanner.nextInt();
-//			o = new Operations(n, k);
-//			o.printString();
-//			while(true) {
-//				System.out.print("Flip (-1 to exit):");
-//				int i = scanner.nextInt();
-//				if(i == -1)
-//					break;
-//				o.change(i-1);
-//				o.printString();
-//			}
-//			o.solve();
-//		}
-		randomTest(10);
-
+		Scanner scanner = new Scanner(System.in);
+		outerloop:
+			while(true) {
+				String current;
+				if(prototype) current = "Prototype Algorithm";
+				else current = "Proven Algorithm";
+				System.out.println("\nEnter test option (-1 to exit):\n1) See our hand-written tests\n2) Do tests with random n, k and cost values (Does " + TEST_COUNT + " tests by default)\n"
+						+ "3) Enter n, k and costs for custom test\n4) Switch mode to use prototype or proven algorithm (Current: " + current + ")\n5) Compare two algorithms on 100 random inputs");
+				int choice = scanner.nextInt();
+				switch(choice) {
+				case 1:
+					System.out.println("\nHand-written tests: ");
+					handWrittenTest();
+					break;
+				case 2:
+					System.out.println("\nDoing " + TEST_COUNT + " random tests: ");
+					randomTest(TEST_COUNT);
+					break;
+				case 3:
+					System.out.println("\nCustom test: ");
+					customTest(scanner);
+					break;
+				case 4:
+					if(prototype) System.out.println("\nChanging mode to proven algorithm.");
+					else System.out.println("\nChanging mode to prototype.");
+					prototype = !prototype;
+					break;
+				case 5:
+					compareAlgorithms(100);
+					break;
+				case -1:
+					break outerloop;
+				default:
+					System.out.println("\nInvalid input. Please enter 1, 2, or 3 (-1 to exit).");
+					break;
+				}
+			}
+		System.out.println("\nExiting.");
 	}
-	
-	public static void randomTest(int tries) {
+
+
+	private static void handWrittenTest() {
+		int minCost, fails = 0;
+		long startTime, elapsedTime;
+		System.out.println("\n///////////////////////////////\nOption 1: 2k+1|n-1\n///////////////////////////////\n");
+		Operations o = new Operations(21, 2, new int[]{1, 5, 3, 2, 4, 6, 8, 4, 5, 1, 0, 2, 1, 3, 1, 1, 4, 7, 5, 1, 4});
+		System.out.println("n: 21 k: 2");
+		startTime = System.nanoTime();
+		minCost = o.optionSolve(prototype);
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("The minimum cost should be 12, and it is: " + minCost + "\nThe test took " + elapsedTime + " milliseconds.");
+		if(minCost != 12)	fails++;
+		o = new Operations(22, 3, new int[] {3, 5, 3, 2, 4, 6, 8, 4, 5, 1, 0, 2, 1, 3, 1, 1, 4, 7, 5, 1, 4, 6});
+		System.out.println("n: 22 k: 3");
+		startTime = System.nanoTime();
+		minCost = o.optionSolve(prototype);
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("The minimum cost should be 14, and it is: " + minCost + "\nThe test took " + elapsedTime + " milliseconds.");
+		if(minCost != 14)	fails++;
+		System.out.println("\n///////////////////////////////\nOption 2: n%(2k+1) < k+1\n///////////////////////////////\n");
+		o = new Operations(17, 3, new int[] {4, 2, 9, 8, 4, 2, 6, 2, 9, 12, 14, 15, 18, 9, 0, 14, 17});
+		System.out.println("n: 17 k: 3");
+		startTime = System.nanoTime();
+		minCost = o.optionSolve(prototype);
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("The minimum cost should be 6, and it is: " + minCost + "\nThe test took " + elapsedTime + " milliseconds.");
+		if(minCost != 6)	fails++;
+		o = new Operations(11, 4, new int[]{2, 1, 11, 4, 7, 13, 6, 12, 21, 5, 11});
+		System.out.println("n: 11 k: 4");
+		startTime = System.nanoTime();
+		minCost = o.optionSolve(prototype);
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("The minimum cost should be 7, and it is: " + minCost + "\nThe test took " + elapsedTime + " milliseconds.");
+		if(minCost != 7)	fails++;
+		System.out.println("\n///////////////////////////////\nOption 3: n%(2k+1) = k+1\n///////////////////////////////\n");
+		o = new Operations(18, 3, new int[] {4, 2, 5, 9, 1, 11, 4, 16, 8, 6, 5, 13, 1, 0, 1, 2, 14, 15});
+		System.out.println("n: 18 k: 3");
+		startTime = System.nanoTime();
+		minCost = o.optionSolve(prototype);
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("The minimum cost should be 12, and it is: " + minCost + "\nThe test took " + elapsedTime + " milliseconds.");
+		if(minCost != 12)	fails++;
+		System.out.println("\n///////////////////////////////\nOption 4: n%(2k+1) > k+1\n///////////////////////////////\n");
+		o = new Operations(19, 3, new int[] {4, 2, 9, 8, 4, 2, 6, 2, 9, 12, 14, 15, 18, 9, 0, 14, 17, 14, 2});
+		System.out.println("n: 19 k: 3");
+		startTime = System.nanoTime();
+		minCost = o.optionSolve(prototype);
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("The minimum cost should be 25, and it is: " + minCost + "\nThe test took " + elapsedTime + " milliseconds.");
+		if(minCost != 25)	fails++;
+		o = new Operations(16, 4, new int[]{2, 1, 11, 4, 7, 13, 6, 12, 21, 5, 11, 5, 2, 4, 12, 2});
+		System.out.println("n: 16 k: 4");
+		startTime = System.nanoTime();
+		minCost = o.optionSolve(prototype);
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("The minimum cost should be 6, and it is: " + minCost + "\nThe test took " + elapsedTime + " milliseconds.");
+		if(minCost != 6)	fails++;
+		System.out.println("\n///////////////////////////////\nOption 5: n <= k+1\n///////////////////////////////\n");
+		o = new Operations(20, 19, new int[] {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 0, 20, 30, 40, 50, 60, 70, 80, 90, 100});
+		System.out.println("n: 20 k: 19");
+		startTime = System.nanoTime();
+		minCost = o.optionSolve(prototype);
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("The minimum cost should be 0, and it is: " + minCost + "\nThe test took " + elapsedTime + " milliseconds.");
+		if(minCost != 0)	fails++;
+		o = new Operations(30, 29, new int[] {10, 20, 30, 40, 50, 60, 70, 80, 90, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100});
+		System.out.println("n: 30 k: 29");
+		startTime = System.nanoTime();
+		minCost = o.optionSolve(prototype);
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("The minimum cost should be 9, and it is: " + minCost + "\nThe test took " + elapsedTime + " milliseconds.");
+		if(minCost != 9)	fails++;
+		System.out.println("\n///////////////////////////////\nOption 6: k = 0\n///////////////////////////////\n");
+		o = new Operations(10, 0, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+		System.out.println("n: 10 k: 0");
+		startTime = System.nanoTime();
+		minCost = o.optionSolve(prototype);
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("The minimum cost should be 55, and it is: " + minCost + "\nThe test took " + elapsedTime + " milliseconds.");
+		if(minCost != 55)	fails++;
+		if(fails == 0) System.out.println("\nAll tests passed successfully.\n");
+		else System.out.println("\nThere were " + fails + " fails.\n");
+	}
+	private static void randomTest(int tries) {
 		for(int i = 0; i < tries; i++) {
-			int n = (int) (Math.random()*190+10);
+			int n = (int) (Math.random()*40+10);
 			int k = (int) (Math.random()*17+3);
 			System.out.println("\n\n\nTest #" + (i+1) + " n: " + n + " k: " + k+ ".");
 			int[] costs = new int[n];
 			for(int j = 0; j < n; j++) {
 				costs[j] = (int) (Math.random()*100);	//Costs are randomized between 0 and 100;
+				if( j != n-1) System.out.print(costs[j] + ", ");
+				else System.out.println(costs[j]);
 			}
 			Operations o = new Operations(n, k, costs);
-			System.out.println("The minimum costs is: " + o.solve(true));
+			long startTime = System.nanoTime();
+			System.out.println("The minimum costs is: " + o.optionSolve(prototype));
+			long elapsedTime = System.nanoTime() - startTime;
+			System.out.println("The test took " + elapsedTime + " milliseconds.");
 		}
+	}
+	private static void customTest(Scanner sc) {
+		System.out.println("Enter n: ");
+		int n = sc.nextInt();
+		System.out.println("Enter k: ");
+		int k = sc.nextInt();
+		System.out.println("Enter costs as an n bit string: ");
+		sc.nextLine();
+		String costsStr = sc.nextLine();
+		while(costsStr.length() != n) {
+			System.out.println("Invalid string length. Please enter a string of lenth n: ");
+			costsStr = sc.nextLine();
+		}
+		int[] costs = new int[n];
+		for(int i = 0; i < n; i++) {
+			costs[i] = Character.getNumericValue(costsStr.charAt(i));	//Put the String in an array.
+		}
+		Operations o = new Operations(n, k, costs);
+		long startTime = System.nanoTime();
+		System.out.println("The minimum costs is: " + o.optionSolve(prototype));
+		long elapsedTime = System.nanoTime() - startTime;
+		System.out.println("The test took " + elapsedTime + " milliseconds.");
+	}
+	private static void compareAlgorithms(int tries) {
+		for(int i = 0; i < 100; i++) {
+			int n = (int) (Math.random()*190+10);
+			int k = (int) (Math.random()*27+3);
+			int[] costs = new int[n];
+			for(int j = 0; j < n; j++) {
+				costs[j] = (int) (Math.random()*100);	//Costs are randomized between 0 and 100;
+			}
+			Operations o = new Operations(n, k, costs);
+			if(!o.compareAlgorithms()) {
+				System.out.println("The algorithms resulted in a different result at n: " + n + " k: " + k + " costs: " + Arrays.toString(costs));
+				return;
+			}
+		}
+		System.out.println("Two algorithms gave the same solution for all " + tries + " tests.");
 	}
 }
